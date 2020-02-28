f23b79bc40a2aede19fafe838736b4c1dbbfa252
Mariana Cid <cidmariana5@gmail.com>
2020-02-28 10:22:28 -0700

    Test and class created to create key object.

---
+++ test/key_test.rb
@@ -0,0 +1,12 @@
+require './lib/key'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class KeyTest < Minitest::Test
+
+  def test_it_exists
+    key = Key.new
+
+    assert_instance_of Key, key
+  end
+end
