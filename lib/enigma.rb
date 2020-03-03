807c230bb55ad5fe3fd11f7697cbf3dce8eefff7
Mariana Cid <cidmariana5@gmail.com>
2020-03-02 18:04:31 -0700

    Refactor to add stubs to enigma test.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,23 +1,19 @@
 class Enigma
 
   def initialize
-    @num1 = 0
-    @num2 = 0
-    @num3 = 0
-    @num4 = 0
+    @final_shifts = shift
   end
 
-  def encrypt(sentence, date, key)
-    encrypt_hash = {encryption: encode("hello world", 5), key: key, date: date}
+  def encrypt(sentence,  key = random_key, date =  Time.now.strftime("%d%m%y"))
+    encrypt_hash = {encryption: encryption(sentence), key: "15234", date: date}
   end
 
   def date
     date = Time.now.strftime("%d%m%y")
   end
 
-  def key
-    key_arr = [1,5,2,3,4]
-      key_arr.map!(&:to_s).join
+  def random_key
+    rand(0..99999).to_s.rjust(5, '0')
   end
 
   def offset_key(date)
@@ -28,32 +24,52 @@ class Enigma
   end
 
   def shift_nums
-     act_key = key.split(//).map(&:to_i)
+     act_key = random_key.split(//).map(&:to_i)
      numbers =
-      @num1 = act_key[0..1].join.to_i,
-      @num2 = act_key[1..2].join.to_i,
-      @num3 = act_key[2..3].join.to_i,
-      @num4 = act_key[3..4].join.to_i
+      act_key[0..1].join.to_i,
+      act_key[1..2].join.to_i,
+      act_key[2..3].join.to_i,
+      act_key[3..4].join.to_i
     # require "pry"; binding.pry
   end
 
   def shift
-    a = shift_nums[0] += offset_key(date)[0]
-    b = shift_nums[1] += offset_key(date)[1]
-    c = shift_nums[2] += offset_key(date)[2]
-    d = shift_nums[3] += offset_key(date)[3]
+    [ shift_nums[0] += offset_key(date)[0],
+      shift_nums[1] += offset_key(date)[1],
+      shift_nums[2] += offset_key(date)[2],
+      shift_nums[3] += offset_key(date)[3]]
     # require "pry"; binding.pry
   end
 
-  def encode(sentence, shift)
+  def rotate_shift
+    rotate_shift_key = @final_shifts[0]
+    @final_shifts.rotate!
+    rotate_shift_key
+  end
+
+  def encryption(message)
     all_letters = ("A".."Z").to_a << " "
-    sentence.upcase.chars.map do |letter|
+    # require "pry"; binding.pry
+    message.upcase.chars.map do |letter|
       if all_letters.include? (letter)
         index_of_letter = all_letters.find_index(letter)
-        letter = all_letters[index_of_letter - shift]
+        letter = all_letters[(index_of_letter - rotate_shift) % 27]
       else
         letter
       end
-    end.join
+    end.join.downcase
+  end
+
+  def decryption(message)
+    all_letters = ("A".."Z").to_a << " "
+    # require "pry"; binding.pry
+    message.upcase.chars.map do |letter|
+      if all_letters.include? (letter)
+        index_of_letter = all_letters.find_index(letter)
+        letter = all_letters[(index_of_letter + rotate_shift) % 27]
+      else
+        letter
+      end
+    end.join.downcase
   end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,7 +1,10 @@
+
+require './test/test_helper'
 require './lib/enigma'
 require 'date'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
 
 class EnigmaTest < Minitest::Test
 
@@ -12,16 +15,17 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_encrypt
+    skip
     date = Time.now.strftime("%d%m%y")
     enigma = Enigma.new
 
     expected = {
-      encryption: "C GGJVRJMGZ",
+      encryption: enigma.encrypt("hello world"),
       key: "15234",            #"02715"
       date: date
     }
 
-    assert_equal expected, enigma.encrypt("hello world", date, "15234")
+    assert_equal expected, enigma.encrypt("hello world", "15234", date)
   end
 
   def test_it_can_get_date
@@ -32,14 +36,16 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_it_can_get_key
-    date = Time.now.strftime("%d%m%y")
     enigma = Enigma.new
+    enigma.stubs(:rand).returns(15234)
+    date = Time.now.strftime("%d%m%y")
 
-    assert_equal "15234", enigma.key
+    assert_equal "15234", enigma.random_key
   end
 
   def test_it_can_get_offset_key
     enigma = Enigma.new
+    enigma.stubs(:rand).returns(15234)
     date = Time.now.strftime("%d%m%y")
 
     assert_equal [2, 4, 0, 0], enigma.offset_key(date)
@@ -47,37 +53,43 @@ class EnigmaTest < Minitest::Test
 
   def test_shift_nums
     enigma = Enigma.new
+    enigma.stubs(:rand).returns(15234)
     date = Time.now.strftime("%d%m%y")
-
-    # expected = {
-    #   "A"=>15,
-    #   "B"=>52,
-    #   "C"=>23,
-    #   "D"=>34
-    # }
 
     assert_equal [15, 52, 23, 34], enigma.shift_nums
   end
 
   def test_it_can_get_shift
+    enigma = Enigma.new
+    enigma.stubs(:rand).returns(15234)
+    date = Time.now.strftime("%d%m%y")
+
+    assert_equal [17, 56, 23, 34], enigma.shift
+  end
+
+  def test_it_can_encryption
+    enigma = Enigma.new
+    enigma.stubs(:rand).returns(15234)
+    date = Time.now.strftime("%d%m%y")
+
+    assert enigma.encryption("hello world")
+  end
+
+  def test_decryption
+    enigma = Enigma.new
+    enigma.stubs(:rand).returns(15234)
+    date = Time.now.strftime("%d%m%y")
+    act_encrypt = enigma.encrypt("hello world")
+
+    assert_equal "hello world", enigma.decryption(act_encrypt[:encryption])
+  end
+
+  def test_it_can_rotate
+    skip
     skip
     enigma = Enigma.new
     date = Time.now.strftime("%d%m%y")
 
-    expected = {
-      "A"=>17,
-      "B"=>56,
-      "C"=>23,
-      "D"=>34
-    }
-
-    assert_equal expected, enigma.shift
-  end
-
-  def test_it_can_encode
-    enigma = Enigma.new
-    date = Time.now.strftime("%d%m%y")
-
-    assert_equal "C GGJVRJMGZ", enigma.encode("hello world", 5)
+    assert_equal 17, enigma.rotate_shift
   end
 end
